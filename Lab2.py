11e11d5eb58aecfe20d8145882494f40ae22c6d4
Hayden <hchalin98@gmail.com>
2025-07-02 16:51:27 -0700

    Lab 2 complete

---
+++ Lab2.py
@@ -0,0 +1,4 @@
+import input_functions
+import output_functions
+user_name = input_functions.get_name()
+output_functions.put_greeting(user_name)
---
+++ output_functions.py
@@ -0,0 +1,2 @@
+def put_greeting(name: str):
+    print(f"Hello {name}!")
